3b35b4eb68f4cb46ef5ffea69eff141f6bbd267f
dino56ok <uhuu11@mailfence.com>
2022-03-03 13:38:15 +0200

    Fix typo

--- data/exits.ts
+++ data/exits.ts
@@ -8,7 +8,7 @@ export const data: DataType[] = [
     subject: 'CyberX arena',
     logo: 'https://cyberxarena.lt/wp-content/uploads/2020/10/Logo_did.png',
     about: 'Laisvalaikio erdvė',
-    connection: 'Paskelbė nuotraukiantys frančizės sutarty su CyberX',
+    connection: 'Paskelbė nuotraukiantys frančizės sutartį su CyberX',
     source: 'https://www.facebook.com/cyberxarena/posts/458205912756083',
   },
 ]
