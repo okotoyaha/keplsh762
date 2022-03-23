8962f1cdf704f613a7974452e2b518779ddc71ba
ObsidianMaw <obsidianmaw@protonmail.com>
2022-03-23 21:54:14 +0100

    Data

--- data/indexGenerated.ts
+++ data/indexGenerated.ts
@@ -806,7 +806,23 @@ export const data: DataType[] = [
           'https://www.cityservice.eu/assets/Uploads/Files/City-Service-SE-2021-Q4-Interim-consolidated-financial-statements.pdf',
       },
     ],
-    support: [],
+    support: [
+      {
+        connection:
+          '"Įmonės akcininkai dar vasario mėn. priėmė sprendimą stabdyti verslą Rusijos rinkoje ir solidarizuotis su Ukrainos bendruomene bei visu vakarų pasauliu"',
+        source: 'https://stoprus.com/assets/cityservice.png',
+      },
+      {
+        connection:
+          '"Skyrėme finansinę paramą SOS Vaikų kaimui, kuris priglaudė ir rūpinasi mažiausiais karo pabėgėliais – Ukrainos vaikais."',
+        source: 'https://tvarumas.cityservice.lt/parama-ukrainos-zmonems/',
+      },
+      {
+        connection:
+          '"Kartu su Nacionaliniu savanorių pagalbos koordinavimo centru „Stiprūs kartu“ pradedame bendradarbiavimą ir padėsime ieškant būstų karo pabėgėliams."',
+        source: 'https://tvarumas.cityservice.lt/parama-ukrainos-zmonems/',
+      },
+    ],
   },
   {
     id: 236,
